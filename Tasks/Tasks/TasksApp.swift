483e056cd0ac630b718118ed2636d24bc1c489e0
Justin Bush <revblaze@users.noreply.github.com>
2021-07-08 14:09:48 -0400

    Start SwiftUI 3 (Xcode 13 beta) variant for new SwipeActions

---
+++ Tasks/Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Justin Bush on 2021-07-08.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
